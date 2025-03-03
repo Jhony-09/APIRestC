02dcd947b1fac8f7f2cc3de26add88b286bf2ded
Jhony Estuardo Mariano Choc Tiul <jhonychocpc@gmail.com>
2025-03-03 17:58:06 -0600

    cambios faltestes modificacion de rutas de acceso

--- obj/Debug/net9.0/webApiTwo.AssemblyInfo.cs
+++ obj/Debug/net9.0/webApiTwo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApiTwo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9c03951a358cd01f6221800ae95c6295d50a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5232bd2fc62acc9df0eb9a4b72005330d0abcab8")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApiTwo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApiTwo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
