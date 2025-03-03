0cd125bba3b3f20393c22c1302576aa3355d120d
Jhony Estuardo Mariano Choc Tiul <jhonychocpc@gmail.com>
2025-03-03 17:23:15 -0600

    Agregar metodos post, delete para modificar la lista del demo de la API REST

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,20 +13,48 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    private static List<WeatherForecast> listWeatherForecasts = new List<WeatherForecast>();
+
+    
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        if (listWeatherForecasts.Count == null || !listWeatherForecasts.Any())
+        {
+            listWeatherForecasts = Enumerable.Range(1, 100).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-0, -0),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToList();
+        }
+        
     }
+    
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+       return listWeatherForecasts;
     }
+
+    [HttpPost]
+    public IActionResult Post( WeatherForecast weatherForecast)
+    {
+        listWeatherForecasts.Add(weatherForecast);
+        return Ok(weatherForecast);
+        
+    }
+
+    [HttpDelete( "{index}")]
+
+    public IActionResult Delete(int index)
+    {
+        listWeatherForecasts.RemoveAt(index);
+        return Ok();
+    }
+
+
 }
